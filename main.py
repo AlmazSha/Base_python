5275cd3b81a393bad4a675c672878f381d64d986
Almaz Shaiakhmetov <sh.almaz87@gmail.com>
2021-10-23 15:37:51 +0500

    1st home work

--- main.py
+++ main.py
@@ -7,8 +7,34 @@ print("Вторая переменная: ", peremennaya_2)
 
 peremennaya_3 = int(input("Введите число: "))
 peremennaya_4 = int(input("Введите ещё число: "))
-
+stroke_1 = input("Введите любое предложение: ")
+peremennaya_3 = str(peremennaya_3)
+peremennaya_4 = str(peremennaya_4)
+print("Вы ввели: ", peremennaya_3,"; ", peremennaya_4,"; ", stroke_1)
 
 
 #2. Пользователь вводит время в секундах. Переведите время в часы, минуты и секунды и выведите в формате чч:мм:сс.
 # Используйте форматирование строк.
+sec = int(input("Введите время в секундах: "))
+sec = sec % (24 * 3600)
+hours = sec // 3600
+sec %= 3600
+minute = sec // 60
+sec %= 60
+user_time = f"Время в формате чч:мм:сс: {hours:02}:{minute:02}:{sec:02}"
+print(user_time)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
